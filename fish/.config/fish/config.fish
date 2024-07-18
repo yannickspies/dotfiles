1c23fe4f177cb26367fa1367afca7c947fbf5a5e
Yannick Spies <yannickspies91@gmail.com>
2024-07-19 00:14:31 +0200

    fixed stow fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+set fish_greeting ""
+
+set -gx EDITOR nvim
+
+set -Ux fish_user_paths $fish_user_paths ~/.local/bin
+
+if type -q eza
+    alias ll "eza -l -g --icons"
+    alias lla "ll -a"
+end
+
+set -gx PNPM_HOME "/home/yspies/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
