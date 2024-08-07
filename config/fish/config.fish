b20d7662214f5b92666fb0b363a03a3435409350
Yannick Spies <yannickspies91@gmail.com>
2024-08-07 14:37:55 +0200

    temporarily removed nvim for clean install

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,8 +8,3 @@ if type -q eza
     alias ll "eza -l -g --icons"
     alias lla "ll -a"
 end
-
-set -gx PNPM_HOME "/home/yspies/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
