f1019f5e1b6c45d436ccac4277dc67eb7188876d
Yannick Spies <yannickspies91@gmail.com>
2024-10-11 13:55:53 +0200

    adding some plugins

--- nvim/.config/nvim/lua/custom/plugins/inc-rename.lua
+++ nvim/.config/nvim/lua/custom/plugins/inc-rename.lua
@@ -1,6 +1,7 @@
 return {
   "smjonas/inc-rename.nvim",
   config = function()
-    require("inc_rename").setup()
-  end,
+    require("inc_rename").setup {
+  input_buffer_type = "dressing",
+}  end,
 }
--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -5,18 +5,33 @@ return {
 
     null_ls.setup({
       sources = {
+        -- Use Flake8 for Python diagnostics
+        -- null_ls.builtins.diagnostics.flake8.with({
+        --   extra_args = { "--max-line-length", "120" },
+        -- }),
+        
+        -- Black for Python formatting
+        null_ls.builtins.formatting.black.with({
+          extra_args = { "--line-length", "120" },
+        }),
+
+        -- Prettier for web-related filetypes
         null_ls.builtins.formatting.prettier.with({
           filetypes = { "javascript", "typescript", "vue", "json", "html", "css", "markdown" },
         }),
-        -- Add Stylua for Lua formatting
-        null_ls.builtins.formatting.stylua,
+
+        -- Stylua for Lua formatting
+        null_ls.builtins.formatting.stylua.with({
+          extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+        }),
       },
     })
 
+    -- Autoformat on save for specified filetypes
     vim.cmd([[
       augroup FormatAutogroup
         autocmd!
-        autocmd BufWritePost *.js,*.ts,*.vue,*.json,*.html,*.css,*.md,*.lua lua vim.lsp.buf.format({ async = true })
+        autocmd BufWritePost *.js,*.ts,*.vue,*.json,*.html,*.css,*.md,*.lua,*.py lua vim.lsp.buf.format({ async = true })
       augroup END
     ]])
   end,
